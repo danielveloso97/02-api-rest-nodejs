40a75b5ae73949b655f64938fa8bff7245694394
Daniel <ds799623@gmail.com>
2024-10-09 17:59:01 -0300

    Configurando o db

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,20 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
+  const transections = await knex('transections')
+    .where('amount', 1000)
+    .select('*')
 
-  return tables
+  return transections
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server Running')
